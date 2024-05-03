10f1e3640f74a7b575c194984d7563a8320c56d7
tschoemaker <tschoemaker@uni-osnabrueck.de>
2024-05-03 10:42:27 +0200

    with errors

--- src/net/sourceforge/plantuml/klimt/drawing/eps/PostScriptCommandQuadTo.java
+++ src/net/sourceforge/plantuml/klimt/drawing/eps/PostScriptCommandQuadTo.java
@@ -5,12 +5,12 @@
  * (C) Copyright 2009-2024, Arnaud Roques
  *
  * Project Info:  https://plantuml.com
- * 
+ *
  * If you like this project or if you find it useful, you can support us at:
- * 
+ *
  * https://plantuml.com/patreon (only 1$ per month!)
  * https://plantuml.com/paypal
- * 
+ *
  * This file is part of PlantUML.
  *
  * PlantUML is free software; you can redistribute it and/or modify it
@@ -30,28 +30,27 @@
  *
  *
  * Original Author:  Arnaud Roques
- * 
+ *
  *
  */
 package net.sourceforge.plantuml.klimt.drawing.eps;
 
+import net.sourceforge.plantuml.activitydiagram3.ftile.RectangleCoordinates;
+
 public class PostScriptCommandQuadTo implements PostScriptCommand {
 
-	private final double x1;
-	private final double y1;
-	private final double x2;
-	private final double y2;
+	private RectangleCoordinates rectangleCoordinates = new RectangleCoordinates(0.0, 0.0, 0.0, 0.0);
 
 	public PostScriptCommandQuadTo(double x1, double y1, double x2, double y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
+		this.rectangleCoordinates.setX1(x1);
+		this.rectangleCoordinates.setY1(y1);
+		this.rectangleCoordinates.setX2(x2);
+		this.rectangleCoordinates.setY2(y2);
 	}
 
 	public String toPostString() {
-		return EpsGraphics.format(x1) + " " + EpsGraphics.format(y1) + " " + EpsGraphics.format(x2) + " "
-				+ EpsGraphics.format(y2) + " rquadto";
+		return EpsGraphics.format(rectangleCoordinates.getX1()) + " " + EpsGraphics.format(rectangleCoordinates.getY1()) + " " + EpsGraphics.format(rectangleCoordinates.getX2()) + " "
+						+ EpsGraphics.format(rectangleCoordinates.getY2()) + " rquadto";
 	}
 
 }
